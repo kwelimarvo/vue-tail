efb94554d442c8c0d60f9e50836adb839f395220
kwelimarvo <marvin@kwelicapital.com>
2022-06-13 21:31:22 +0300

    Tailwind Configured

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div class="container mx-auto bg-amber-400">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
